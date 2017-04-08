0c28c6626eba4181ad144da880e2999c04b3686c
Stoyan Delev <s.delev@elements.nl>
2017-04-08 13:02:44 +0200

    Add model and types

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,144 @@
 module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Html.Events exposing (onClick)
+import GraphQL.Request.Builder exposing (..)
+import GraphQL.Request.Builder.Arg as Arg
+import GraphQL.Request.Builder.Variable as Var
+import Array
+
 
 -- component import example
-import Components.Hello exposing ( hello )
+-- import Components.Hello exposing ( hello )
+-- TYPES
+
+
+type alias Event =
+    { id : String
+    , imgUrl : String
+    , location : String
+    , name : String
+    , place : String
+    , color : String
+    }
+
+
+
+-- FAKE DATA
+
+
+radiohead : Event
+radiohead =
+    { id = "2", imgUrl = "2.jpg", location = "Drente", name = "Radiohead", place = "Rose", color = "red" }
+
+
+muse : Event
+muse =
+    { id = "1", imgUrl = "1.jpg", location = "Amsterdam", name = "Muse", place = "Ziggo", color = "blue" }
+
+
+rhcp : Event
+rhcp =
+    { id = "3", imgUrl = "3.jpg", location = "Amsterdam", name = "RHCP", place = "Ziggo", color = "pink" }
+
 
 
 -- APP
-main : Program Never Int Msg
+
+
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    { events : List Event
+    , selected : Int
+    }
+
+
+model : Model
+model =
+    { selected = 0
+    , events =
+        []
+        -- rhcp, muse, radiohead ]
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | MoveForward
+    | MoveBack
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        MoveForward ->
+            moveForward model
+
+        MoveBack ->
+            moveBack model
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div []
+        [ button [ class "btn btn--prev", onClick MoveBack ] [ text "Back " ]
+        , div [] [ printEvent <| getEvent model ]
+        , button [ class "btn btn--next", onClick MoveForward ] [ text "Forward " ]
         ]
-      ]
-    ]
-  ]
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+moveForward : Model -> Model
+moveForward model =
+    if List.length model.events - 1 == model.selected then
+        { model | selected = 0 }
+    else
+        { model | selected = model.selected + 1 }
+
+
+moveBack : Model -> Model
+moveBack model =
+    if model.selected == 0 then
+        { model | selected = List.length model.events - 1 }
+    else
+        { model | selected = model.selected - 1 }
+
+
+getEvent : Model -> Maybe Event
+getEvent model =
+    Array.get model.selected <| Array.fromList model.events
+
+
+printEvent : Maybe Event -> Html Msg
+printEvent event =
+    case event of
+        Nothing ->
+            div [ class "error" ] [ text "NO SUCH A EVENT" ]
+
+        Just event ->
+            div [ class "event" ]
+                [ h1 [ class "event__title" ] [ text event.name ]
+                , p [ class "event__place" ] [ text event.place ]
+                , p [ class "event__location" ] [ text event.location ]
+                ]
